e379261d693c6be981423b82d012734192b085d3
NicoVIII <nico@nicoviii.net>
2023-08-25 12:19:39 +0000

    Implement publish and testrun in container

--- backend/run.fs
+++ backend/run.fs
@@ -1,4 +1,6 @@
 open RunHelpers
+open RunHelpers.BasicShortcuts
+open RunHelpers.Shortcuts
 open RunHelpers.Templates
 
 [<RequireQualifiedAccess>]
@@ -17,6 +19,34 @@ module Task =
 
     let run () = job { DotNet.run Config.publishPath }
 
+    let publish version =
+        job {
+            let tags =
+                [
+                    "latest"
+                    match version with
+                    | Some version -> version: string
+                    | None -> ()
+                ]
+                |> String.concat ";"
+
+            dotnet [
+                "publish"
+                Config.publishPath
+                "--os"
+                "linux"
+                "--arch"
+                "x64"
+                "/t:PublishContainer"
+                $"/p:ContainerImageTags=\"{tags}\""
+                "-c"
+                "Release"
+            ]
+        }
+
+    let runContainer () =
+        job { cmd "docker" [ "run"; "-p"; "5000:80"; "complete-information-household-backend:latest" ] }
+
 [<EntryPoint>]
 let main args =
     args
@@ -34,5 +64,16 @@ let main args =
                 Task.restore ()
                 Task.run ()
             }
+        | [ "publish" ] ->
+            job {
+                Task.restore ()
+                Task.publish None
+            }
+        | [ "publish"; version ] ->
+            job {
+                Task.restore ()
+                Task.publish (Some version)
+            }
+        | [ "container" ] -> job { Task.runContainer () }
         | _ -> Job.error [ "Usage: dotnet run [<command>]"; "Look up available commands in run.fs" ]
     |> Job.execute
